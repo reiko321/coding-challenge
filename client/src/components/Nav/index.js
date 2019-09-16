6659ebac49b9750c2f319626cb77d4e2e4e2e689
reiko321 <reiko321@gmail.com>
2019-09-16 14:33:44 -0700

    update

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">
         GoDaddy ATM
       </a>
